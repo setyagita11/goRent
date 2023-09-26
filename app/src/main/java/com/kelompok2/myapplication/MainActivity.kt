26b10d366025e6fc3dceac65d2d7e136b5c46656
riqzamuqtada <riqmuq106@gmail.com>
2023-09-26 23:01:06 +0700

    add viewBinding

--- app/src/main/java/com/kelompok2/myapplication/MainActivity.kt
+++ app/src/main/java/com/kelompok2/myapplication/MainActivity.kt
@@ -4,16 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.kelompok2.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var find : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        find = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(find.root)
 
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, 2000)
+
+        find.textVersion.text = "version " + BuildConfig.VERSION_NAME
 
     }
 }
