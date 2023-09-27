8513cf8ac6781ec4108a97062df0d3a2a36c4a8d
riqzamuqtada <riqmuq106@gmail.com>
2023-09-27 22:20:49 +0700

    logika login

--- app/src/main/java/com/kelompok2/myapplication/LoginActivity.kt
+++ app/src/main/java/com/kelompok2/myapplication/LoginActivity.kt
@@ -1,11 +1,43 @@
 package com.kelompok2.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kelompok2.myapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var find : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        find = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(find.root)
+
+        val username  = find.inputUsername
+        val password = find.inputPassword
+
+        find.btnLogin.setOnClickListener {
+
+            if (username.text.isNotEmpty() && password.text.isNotEmpty()) {
+                startActivity(
+                    Intent(this, DashboardActivity::class.java)
+                        .putExtra("username", username.text)
+                )
+                alert("Selamat datang di Go-Rent ${username.text}")
+                finish()
+            } else {
+                alert("Username dan Password tidak boleh kosong!")
+            }
+
+        }
+
+    }
+
+    private fun alert(msg: String) {
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
     }
 }
