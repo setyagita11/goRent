ad0757ce73ac9a29eaf4e5cc126c044b138b8ff3
NazzilExe <nazililmi57@gmail.com>
2023-09-29 10:42:54 +0700

    all layout za

---
+++ app/src/main/java/com/kelompok2/myapplication/DetailPesananActivity.kt
@@ -0,0 +1,17 @@
+package com.kelompok2.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.kelompok2.myapplication.databinding.ActivityDetailPesananBinding
+
+class DetailPesananActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_pesanan)
+    }
+}
