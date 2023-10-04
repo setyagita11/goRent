19618e583dd5559f57b357df664b1ad6439a1a4b
NazzilExe <nazililmi57@gmail.com>
2023-10-04 09:09:12 +0700

    entity

---
+++ app/src/main/java/com/kelompok2/myapplication/GoRent/Pesanan.kt
@@ -0,0 +1,17 @@
+package com.kelompok2.myapplication.GoRent
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = "pesanan")
+data class Pesanan(
+
+    @PrimaryKey( autoGenerate = true)
+    @ColumnInfo(name = "id") val id : Int,
+    @ColumnInfo(name = "nama pesanan") val namapesanan: String,
+    @ColumnInfo(name = "alamat") val alamat : String,
+    @ColumnInfo(name = "kendaraan") val kendaraan: String,
+    @ColumnInfo(name = "sewa waktu") val sewawaktu : Int,
+    @ColumnInfo(name = "status") val status : String,
+)
