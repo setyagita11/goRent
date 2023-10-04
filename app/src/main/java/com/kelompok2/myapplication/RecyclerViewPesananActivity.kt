f87a24b747c6ee68f43c103d0a87369004ad171e
NazzilExe <nazililmi57@gmail.com>
2023-10-04 08:25:10 +0700

    new layout 2

---
+++ app/src/main/java/com/kelompok2/myapplication/RecyclerViewPesananActivity.kt
@@ -0,0 +1,11 @@
+package com.kelompok2.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecyclerViewPesananActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recyclerview_pesanan)
+    }
+}
