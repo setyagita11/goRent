1eb5070077b501e6a082d584e94f9e498a3789d0
riqzamuqtada <riqmuq106@gmail.com>
2023-10-04 13:32:31 +0700

    test

--- app/src/main/java/com/kelompok2/myapplication/GoRent/kendaraan.kt
+++ app/src/main/java/com/kelompok2/myapplication/GoRent/kendaraan.kt
@@ -5,12 +5,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "Kendaraan")
+@Entity(tableName = "kendaraan")
 data class Kendaraan(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")         val id         : Int,
-    @ColumnInfo (name = "merk")      val merk       : String,
-    @ColumnInfo(name = "jenis")      val jenis      : String,
-    @ColumnInfo(name = "harga_sewa") val hargasewa  : Int,
+    @ColumnInfo(name = "id") val id : Int,
+    @ColumnInfo (name = "merk") val merk : String,
+    @ColumnInfo(name = "harga") val harga : Int,
+    @ColumnInfo(name = "jenis") val jenis : String,
+    @ColumnInfo(name = "harga sewa") val hargasewa : Int,
     @ColumnInfo(name = "persediaan") val persediaan : Int,
 )
