9c77490507d6a40db07cb397f61a0dedefbf8a90
riqzamuqtada <riqmuq106@gmail.com>
2023-10-04 13:36:14 +0700

    kendaraan v2

--- app/src/main/java/com/kelompok2/myapplication/GoRent/Kendaraan.kt
+++ app/src/main/java/com/kelompok2/myapplication/GoRent/Kendaraan.kt
@@ -8,7 +8,7 @@ import androidx.room.PrimaryKey
 data class Kendaraan(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")         val id         : Int,
-    @ColumnInfo(name = "merk")      val merk       : String,
+    @ColumnInfo(name = "merk")      val merk        : String,
     @ColumnInfo(name = "jenis")      val jenis      : String,
     @ColumnInfo(name = "harga_sewa") val hargasewa  : Int,
     @ColumnInfo(name = "persediaan") val persediaan : Int,
