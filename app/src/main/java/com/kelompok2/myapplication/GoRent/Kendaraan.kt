05bb67332326a1f8bbfa0754cb07f0a83e222462
NazzilExe <nazililmi57@gmail.com>
2023-10-04 13:44:20 +0700

    entity

--- app/src/main/java/com/kelompok2/myapplication/GoRent/Kendaraan.kt
+++ app/src/main/java/com/kelompok2/myapplication/GoRent/Kendaraan.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Kendaraan")
+@Entity(tableName = "kendaraan")
 data class Kendaraan(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")         val id         : Int,
