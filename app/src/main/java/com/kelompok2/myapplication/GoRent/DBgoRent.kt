e85cd4533d383271365f33ebb1d268cec0a77175
riqzamuqtada <riqmuq106@gmail.com>
2023-10-04 13:35:00 +0700

    kendaraan

--- app/src/main/java/com/kelompok2/myapplication/GoRent/DBgoRent.kt
+++ app/src/main/java/com/kelompok2/myapplication/GoRent/DBgoRent.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Pesanan::class, Kendaraan::class], version = 2)
+@Database(entities = [Pesanan::class, Kendaraan::class], version = 3)
 abstract class DBgoRent : RoomDatabase(){
 
     abstract fun dao() : DAO
