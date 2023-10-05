0a08dca867b061acf372ca402936a145853c71a9
NazzilExe <nazililmi57@gmail.com>
2023-10-05 11:21:33 +0700

    floatching tambah

--- app/src/main/java/com/kelompok2/myapplication/InputKendaraanActivity.kt
+++ app/src/main/java/com/kelompok2/myapplication/InputKendaraanActivity.kt
@@ -1,11 +1,27 @@
 package com.kelompok2.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kelompok2.myapplication.databinding.ActivityInputKendaraanBinding
+import com.kelompok2.myapplication.databinding.ActivityInputPesananBinding
 
 class InputKendaraanActivity : AppCompatActivity() {
+
+    private lateinit var find : ActivityInputKendaraanBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input_kendaraan)
+        find= ActivityInputKendaraanBinding.inflate(layoutInflater)
+        setContentView(find.root)
+
+        find.btnHome.setOnClickListener { onBackPressed() }
+
+        find.btnPesanan.setOnClickListener{
+            startActivity(
+                Intent(this, RecyclerViewPesananActivity::class.java)
+            )
+        }
+
     }
 }
--- app/src/main/java/com/kelompok2/myapplication/InputPesananActivity.kt
+++ app/src/main/java/com/kelompok2/myapplication/InputPesananActivity.kt
@@ -2,10 +2,17 @@ package com.kelompok2.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kelompok2.myapplication.databinding.ActivityInputPesananBinding
 
 class InputPesananActivity : AppCompatActivity() {
+
+    private lateinit var find : ActivityInputPesananBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input_pesanan)
+        find= ActivityInputPesananBinding.inflate(layoutInflater)
+        setContentView(find.root)
+
+        find.btnHome.setOnClickListener { onBackPressed() }
+
     }
 }
