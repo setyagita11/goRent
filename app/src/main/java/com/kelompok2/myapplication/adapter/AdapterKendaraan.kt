91973b8a321671a22b6b8049d11cef5feb29a894
rehann56 <bossapu13@gmail.com>
2023-10-11 08:28:19 +0700

    var

--- app/src/main/java/com/kelompok2/myapplication/adapter/AdapterKendaraan.kt
+++ app/src/main/java/com/kelompok2/myapplication/adapter/AdapterKendaraan.kt
@@ -8,12 +8,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kelompok2.myapplication.R
 import java.util.*
 
-class AdapterKendaraan (val list: ArrayList<Kendaraan>) : RecyclerView.Adapter<AdapterKendaraan.ViewHolder>() {
+class AdapterKendaraan (val list: ArrayList<Kendaraan> val listener: kendaraan) : RecyclerView.Adapter<AdapterKendaraan.ViewHolder>() {
     class ViewHolder (view: View):RecyclerView.ViewHolder(view) {
 
         val merek = itemView.findViewById<TextView>(R.id.txtMerek)
         val harga = itemView.findViewById<TextView>(R.id.txtHarga)
         val tersedia = itemView.findViewById<TextView>(R.id.txtTersedia)
+        var listener
 
     }
 
@@ -28,6 +29,7 @@ class AdapterKendaraan (val list: ArrayList<Kendaraan>) : RecyclerView.Adapter<A
         holder.merek.text = list[position].merk
         holder.harga.text = list[position].harga_sewa.toString()
         holder.tersedia.text = list[position].persediaan.toString()
+
     }
 
     fun setData(newList: List<Kendaraan>){
