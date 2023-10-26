bfbedfe8dfb048e6f0c006259b2284845f619ae1
firlina <firlinara21@gmail.com>
2023-10-26 11:18:01 +0700

    code delete pesanan

--- app/src/main/java/com/kelompok2/myapplication/adapter/AdapterPesanan.kt
+++ app/src/main/java/com/kelompok2/myapplication/adapter/AdapterPesanan.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
@@ -13,17 +14,20 @@ import com.kelompok2.myapplication.GoRent.Pesanan
 import com.kelompok2.myapplication.R
 import java.util.ArrayList
 
-class AdapterPesanan (val list: ArrayList<Pesanan>) :
-    RecyclerView.Adapter<AdapterPesanan.ViewHolder>() {
+class AdapterPesanan (val list: ArrayList<Pesanan>,val listener:pesanan)
+    : RecyclerView.Adapter<AdapterPesanan.ViewHolder>() {
     class ViewHolder (view: View):RecyclerView.ViewHolder(view) {
 
         val namaPemesan = itemView.findViewById<TextView>(R.id.txtNama)
         val pesanKndraan = itemView.findViewById<TextView>(R.id.txtKendaraan)
         val alamatPemesan = itemView.findViewById<TextView>(R.id.txtAlamat)
         val durasiWaktu = itemView.findViewById<TextView>(R.id.txtDurasi)
-
         val detail = itemView.findViewById<CardView>(R.id.btnDtailPsnan)
+        val delete = itemView.findViewById<ImageView>(R.id.imgHapusPsnan)
 
+    }
+    interface pesanan{
+        fun hapus(pesanan: Pesanan)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -38,6 +42,9 @@ class AdapterPesanan (val list: ArrayList<Pesanan>) :
         holder.alamatPemesan.text = list[position].alamat
         holder.pesanKndraan.text = list[position].kendaraan
         holder.durasiWaktu.text = list[position].waktu_sewa.toString()
+        holder.delete.setOnClickListener{
+            listener.hapus(list[position])
+        }
 
         holder.detail.setOnClickListener {
             val context = holder.itemView.context
